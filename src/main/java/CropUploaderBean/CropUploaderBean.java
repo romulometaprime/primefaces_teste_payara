d2f3d2457b313c76b3ad77ba4bff5f6d0b5eda35
Rômulo <romulo@metaprime.com.br>
2024-01-29 16:42:52 -0300

    first commit

---
+++ src/main/java/CropUploaderBean/CropUploaderBean.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CropUploaderBean;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.CroppedImage;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.file.UploadedFile;
+
+/**
+ *
+ * @author romulo
+ */
+@Named
+@SessionScoped
+public class CropUploaderBean implements Serializable {
+
+    private CroppedImage croppedImage;
+
+    private UploadedFile originalImageFile;
+
+    public CroppedImage getCroppedImage() {
+        return croppedImage;
+    }
+
+    public void setCroppedImage(CroppedImage croppedImage) {
+        this.croppedImage = croppedImage;
+    }
+
+    public UploadedFile getOriginalImageFile() {
+        return originalImageFile;
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        this.originalImageFile = null;
+        this.croppedImage = null;
+        UploadedFile file = event.getFile();
+        if (file != null && file.getContent() != null && file.getContent().length > 0 && file.getFileName() != null) {
+            this.originalImageFile = file;
+            FacesMessage msg = new FacesMessage("Successful", this.originalImageFile.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public void crop() {
+        if (this.croppedImage == null || this.croppedImage.getBytes() == null || this.croppedImage.getBytes().length == 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Cropping failed."));
+        }
+        else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success",
+                    "Cropped successfully."));
+        }
+    }
+
+    public StreamedContent getImage() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (originalImageFile == null
+                            || originalImageFile.getContent() == null
+                            || originalImageFile.getContent().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(originalImageFile.getContent());
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    public StreamedContent getCropped() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (croppedImage == null
+                            || croppedImage.getBytes() == null
+                            || croppedImage.getBytes().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(this.croppedImage.getBytes());
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+}
